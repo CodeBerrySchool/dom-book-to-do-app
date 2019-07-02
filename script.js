5e916e8e14bc8abbacacf03edc9d59b61abb09cd
laszlovad <laszlo.a.vad@gmail.com>
2019-07-02 09:55:58 +0200

    Refactor the addTodo() function

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-// Create the todoList function - It should run after a click on  the Add button.
 var app = {
-
-	
-	// Handlers
-
-	//Create the add function
 	addTodo: function() {
-	    var inputValue    = document.getElementById('todoInput').value; // Store user input in a variable
-	    var text    = document.createTextNode(inputValue);  // Create a text node from the user input
-		var newItem = document.createElement('li');			// Create a li tag from the text node
-		var todoList = document.getElementById('todoList'); // Store the list element in a variable for later
-	
-	    newItem.appendChild(text); 					// Append text to newItem
-		    if (inputValue === '') {				// Make sure that empty field is not "submittable"
-				alert("Valamit be kell írnod!");
-	  	    } else {
-			    todoList.appendChild(newItem); // Append the li to the todoList
-                newItem.className = 'newItem'; // Adds a class to the list items
-				document.getElementById('todoInput').value = ''; // Reset empty input field after submit
-				app.addCompleteButton();
-				app.addRemoveButton();
-             }
+	    var userInput = document.getElementById('user-input').value;
+	    var todoText = document.createTextNode(userInput);
+		var todoItem = document.createElement('li');
+        todoItem.appendChild(todoText);
+
+		var todoList = document.getElementById('todo-list');
+
+        if (userInput === '') {
+            alert("Valamit be kell írnod!");
+        } else {
+            todoList.appendChild(todoItem);
+            todoItem.className = 'todo-item';
+            app.addCompleteButton();
+            app.addRemoveButton();
+            /* Reset form field to empty. */
+            document.getElementById('user-input').value = '';
+        }
 	},
-	  
-	// Create the remove function
 	removeTodo: function() {
-        var newItem = this.parentNode;
-		newItem.remove();
+        var todoItem = this.parentNode;
+		todoItem.remove();
     },
     
     // Create the complete function
@@ -58,7 +52,7 @@ var app = {
         var removeButton = document.createElement('button'); // Creates the button.
 	    removeButton.textContent = '\u00D7'; // Sets its text.
 	    removeButton.className = 'removeButton'; // Adds a class to it -for later JS.
-		var x = document.getElementsByClassName('newItem');
+		var x = document.getElementsByClassName('todo-item');
 		var i;
 		for (i = 0; i < x.length; i++) {
             x[i].appendChild(removeButton);
@@ -71,7 +65,7 @@ var app = {
 		var completeButton = document.createElement('button'); // Creates the button.
 		completeButton.textContent = '\u2713'; // Sets its text.
 		completeButton.className = 'completeButton'; // Adds a class to it.
-		var x = document.getElementsByClassName('newItem');
+		var x = document.getElementsByClassName('todo-item');
 		var i;
 		for (i = 0; i < x.length; i++) {
   			x[i].appendChild(completeButton);
@@ -83,4 +77,4 @@ var app = {
 
 // Attach the Enter key press to the add function
 
-document.getElementById('todoInput').addEventListener('keypress', app.addOnEnter);
+document.getElementById('user-input').addEventListener('keypress', app.addOnEnter);
